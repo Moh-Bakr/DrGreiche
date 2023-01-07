a433eee408f3ca256e92e8c0bab32535e0d0bad8
Mohamed Bakr <mohamed.bakr@bld.ai>
2023-01-07 13:28:02 +0200

    enable cors and make swagger available in production

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,24 +1,38 @@
 using Backend.Data;
 using Backend.Services.Clients;
 
+var policyName = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: policyName,
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<DataContext>();
 builder.Services.AddTransient<IClientService, ClientService>();
+
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     
+// }
 
 app.UseHttpsRedirection();
 
