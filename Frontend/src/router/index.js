9d42bcd2343e115252fb5174729864c6115a9158
Mohamed Bakr <mohamed.bakr@bld.ai>
2023-01-09 20:47:02 +0200

    add icons & router

---
+++ Frontend/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/samples",
+  },
+  {
+    path: "/samples",
+    name: "Samples",
+    component: () => import("../views/Samples.vue"),
+  },
+  {
+    path: "/clients",
+    name: "Clients",
+    component: () => import("../views/Clients.vue"),
+  },
+  {
+    path: "/add-client",
+    name: "AddClient",
+    component: () => import("../views/AddClient.vue"),
+  },
+  {
+    path: "/add-sample",
+    name: "AddSample",
+    component: () => import("../views/AddSample.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
